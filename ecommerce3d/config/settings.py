b90ccee9e3da9a8debc5467af0806d43c0efe9f6
ateetkalkotwar <158085385+ateetkalkotwar@users.noreply.github.com>
2026-06-07 01:15:06 +0530

    Update Django settings with static files and hosts
    Added STATIC_ROOT and ALLOWED_HOSTS settings.

--- ecommerce3d/config/settings.py
+++ ecommerce3d/config/settings.py
@@ -127,4 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 ALLOWED_HOSTS = ['*']
